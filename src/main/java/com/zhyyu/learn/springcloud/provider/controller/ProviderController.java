175ca6ba2856e1332290fd7a7ec220667c04494d
juror.yu <juror.yu@mart.blue>
2019-08-10 17:04:22 +0800

    feign 表单提交配置

--- src/main/java/com/zhyyu/learn/springcloud/provider/controller/ProviderController.java
+++ src/main/java/com/zhyyu/learn/springcloud/provider/controller/ProviderController.java
@@ -2,6 +2,10 @@ package com.zhyyu.learn.springcloud.provider.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author juror
@@ -14,6 +18,20 @@ public class ProviderController {
     @RequestMapping("hello")
     public String hello() {
         return "hello";
+    }
+
+    @RequestMapping("helloObj")
+    public String helloObj(MyObj1 myObj1) {
+        return myObj1.toString();
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    static class MyObj1 {
+        private String key1;
+        private String key2;
     }
 
 }
